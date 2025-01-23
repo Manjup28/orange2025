01f0fed4a1b79505303970b0655afe76196f4b07
Admin <Admin@DESKTOP-QBJ1VQK>
2025-01-23 23:02:22 +0530

    new test case done.

---
+++ src/main/java/tutorial1/demo1.java
@@ -0,0 +1,13 @@
+package tutorial1;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="manjula";
+for(int i=a.length()-1;i>=0;i--) {
+	System.out.println(a.charAt(i));
+}
+	}
+
+}
